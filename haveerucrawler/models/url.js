81ed546619953681fe6f6b2c744a88a5e1ff67ea
Umran Hussein <umran.hussein@gmail.com>
2015-07-25 20:09:45 +0500

    isolated crawler from front end

--- haveerucrawler/models/url.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var urlSchema = new mongoose.Schema({
-	url: {type: String, unique: true, required: true}
-});
-
-var Url = mongoose.model('Url', urlSchema);
-
-module.exports = Url;
