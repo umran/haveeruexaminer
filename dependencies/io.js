a9fdfe977a470fbcf737e374f95605a2e8d5b235
Umran Hussein <umran.hussein@gmail.com>
2015-07-26 21:40:39 +0500

    emit crawler events to web client

--- dependencies/io.js
+++ dependencies/io.js
@@ -2,8 +2,12 @@ module.exports = function(io){
 
 	io.on('connect', function(socket){
 		socket.on('status', function(data){
-			io.emit('message', data);
+			socket.to('clients').emit('status', data);
+			//console.log(data);
+		});
+		socket.on('update', function(data){
+			socket.to('clients').emit('update', data);
+			//console.log(data);
 		});
 	});
-
 }
