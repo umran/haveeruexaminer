a5401b763279cc663f65baf8202d8c0c1a1e3822
Umran Hussein <umran.hussein@gmail.com>
2015-07-28 00:32:14 +0500

    updated socket io implementation

--- dependencies/io.js
+++ dependencies/io.js
@@ -2,15 +2,11 @@ module.exports = function(io){
 
 	io.on('connect', function(socket){
 		socket.on('status', function(data){
-			socket.to('clients').emit('status', data);
+			socket.broadcast.emit('status', data);
 			//console.log(data);
 		});
 		socket.on('update', function(data){
-			socket.to('clients').emit('update', data);
-			//console.log(data);
-		});
-		socket.on('exception', function(data){
-			socket.to('clients').emit('exception', data);
+			socket.broadcast.emit('update', data);
 			//console.log(data);
 		});
 	});
