62f2486e6f521a0ba2c65ff94795c106df4da883
Umran Hussein <umran.hussein@gmail.com>
2015-08-03 02:31:56 +0500

    possible timestamp solution

--- routes/getrecent.js
+++ routes/getrecent.js
@@ -4,7 +4,7 @@ var Doc = require('../models/doc.js');
 
 router.get('/', function(req, response, next) {
 
-	Doc.find({}, {hash:1, _id.getTimestamp():1}).sort({$natural:-1}).limit(5).exec(function(err, res){
+	Doc.find({}, {hash:1, _id:1}).sort({$natural:-1}).limit(5).exec(function(err, res){
 		if(err){
 			response.setHeader('Content-Type', 'application/json');
 			response.send(JSON.stringify({
@@ -13,6 +13,16 @@ router.get('/', function(req, response, next) {
 			}));
 			return;
 		}
+		
+		//parse response
+		var pretty = [];
+		res.forEach(function(record){
+			var item = {};
+			item.hash = record.hash;
+			item.timestamp = Date.parse(record._id.getTimestamp());
+			pretty.push(item);
+		});
+		
 		response.setHeader('Content-Type', 'application/json');
 		response.send(JSON.stringify({
 			code: 1,
