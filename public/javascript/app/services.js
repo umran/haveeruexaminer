09d0173b4b2b987234c16b24dae3036e35032106
Umran Hussein <umran.hussein@gmail.com>
2015-08-02 15:25:08 +0500

    even more hopeful

--- public/javascript/app/services.js
+++ public/javascript/app/services.js
@@ -48,3 +48,9 @@ main.directive('ngEnter', function() {
 		});
 	};
 });
+
+main.filter('html', ['$sce', function ($sce) { 
+  return function (text) {
+    return $sce.trustAsHtml(text);
+  };    
+}])
