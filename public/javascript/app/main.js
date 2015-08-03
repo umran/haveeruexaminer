8a5988d8c608fb8d7fa8ad165b4220157f8c956e
Umran Hussein <umran.hussein@gmail.com>
2015-08-04 01:16:02 +0500

    test

--- public/javascript/app/main.js
+++ public/javascript/app/main.js
@@ -59,13 +59,17 @@ main
 	})
 	.controller('searchResults', function($scope, $location, $anchorScroll, Scopes){
 		$scope.$watch(function () { return Scopes.get('meta'); }, function (newValue, oldValue) {
-      if (newValue !== oldValue) $scope.meta = 'fetched '+newValue.hits+' documents in '+(newValue.time/1000)+' seconds ';
+      if (newValue !== oldValue){
+      	$scope.meta = 'fetched '+newValue.hits+' documents in '+(newValue.time/1000)+' seconds ';
+      }
     });
 		$scope.$watch(function () { return Scopes.get('results'); }, function (newValue, oldValue) {
-      if (newValue !== oldValue) $scope.results = newValue;
+      if (newValue !== oldValue){
+      	$scope.results = newValue;
+      	
+      	$location.hash('searchResults');
+    		$anchorScroll();
+    	}
     });
-    
-    $location.hash('searchResults');
-    $anchorScroll();
     
 	});
